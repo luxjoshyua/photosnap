b60139e30627c94c3a28b14337570eee964e170d
luxjoshyua <josh.e.fielding@gmail.com>
2020-10-18 16:47:21 +1100

    added features page

--- webpack.common.js
+++ webpack.common.js
@@ -1,62 +1,70 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [{ loader: "html-loader", options: { minimize: true } }],
-      },
-      {
-        test: /\.(png|jpe?g)/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              name: "./img/[name].[ext]",
-              limit: 10000,
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{ loader: "html-loader", options: { minimize: true } }],
             },
-          },
-          {
-            loader: "img-loader",
-          },
+            {
+                test: /\.(png|jpe?g)/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: "./img/[name].[ext]",
+                            limit: 10000,
+                        },
+                    },
+                    {
+                        loader: "img-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    plugins: ["transform-class-properties"],
+                },
+            },
         ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        query: {
-          plugins: ["transform-class-properties"],
-        },
-      },
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            filename: "./index.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/features.html",
+            filename: "./features.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/pricing.html",
+            filename: "./pricing.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/stories.html",
+            filename: "./stories.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "src/index.html",
-      filename: "./index.html",
-    }),
-    new HtmlWebPackPlugin({
-      template: "src/stories.html",
-      filename: "./stories.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
 };
