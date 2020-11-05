b95e0350e3b43e2d390e49ec046ac2bd287268de
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-11-06 07:46:25 +1100

    pricing animations working

--- src/js/modal/index.js
+++ src/js/modal/index.js
@@ -1,9 +1,22 @@
 const circleSlide = document.querySelector(".inner-circle");
 const sliderBg = document.querySelector(".slider-wrapper");
+const monthyEl = document.querySelector(".pricing-tiles-monthly");
+const yearlyEl = document.querySelector(".pricing-tiles-yearly");
 
 // add event listener to the circle slide
 circleSlide.addEventListener("click", (e) => {
-  circleSlide.classList.toggle("active-circle");
-  sliderBg.classList.toggle("active-bg");
-  // when the user clicks, fade out pricing tiles monthly, fade in pricing tiles yearly
+  if (!circleSlide.classList.contains("active-circle")) {
+    circleSlide.classList.add("active-circle");
+    circleSlide.classList.remove("inactive-circle");
+    sliderBg.classList.add("active-bg");
+    monthyEl.classList.add("inactive-monthly");
+    yearlyEl.classList.add("active-yearly");
+  } else {
+    circleSlide.classList.remove("active-circle");
+    circleSlide.classList.add("inactive-circle");
+    sliderBg.classList.remove("active-bg");
+    monthyEl.classList.remove("inactive-monthly");
+    yearlyEl.classList.add("inactive-yearly");
+    yearlyEl.classList.remove("active-yearly");
+  }
 });
