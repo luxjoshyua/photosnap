dc1a9b46ae5cefb3980b543405cf9cc8aef33078
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-11-09 07:32:37 +1100

    added hamburger nav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import style from "./sass/index.scss";
 import "bootstrap";
 import "./js/modal";
+
+// Hamburger Nav
+const hamburger = document.querySelector(".hamburger-nav");
+const navDom = document.querySelector(".nav-links-mb");
+if (hamburger !== null) {
+  hamburger.addEventListener("click", function () {
+    toggleNav();
+  });
+}
+
+function toggleNav() {
+  hamburger.classList.toggle("active");
+  navDom.classList.toggle("activeNavMb");
+}
--- src/js/modal/index.js
+++ src/js/modal/index.js
@@ -4,19 +4,21 @@ const monthyEl = document.querySelector(".pricing-tiles-monthly");
 const yearlyEl = document.querySelector(".pricing-tiles-yearly");
 
 // add event listener to the circle slide
-circleSlide.addEventListener("click", (e) => {
-  if (!circleSlide.classList.contains("active-circle")) {
-    circleSlide.classList.add("active-circle");
-    circleSlide.classList.remove("inactive-circle");
-    sliderBg.classList.add("active-bg");
-    monthyEl.classList.add("inactive-monthly");
-    yearlyEl.classList.add("active-yearly");
-  } else {
-    circleSlide.classList.remove("active-circle");
-    circleSlide.classList.add("inactive-circle");
-    sliderBg.classList.remove("active-bg");
-    monthyEl.classList.remove("inactive-monthly");
-    yearlyEl.classList.add("inactive-yearly");
-    yearlyEl.classList.remove("active-yearly");
-  }
-});
+if (circleSlide) {
+  circleSlide.addEventListener("click", (e) => {
+    if (!circleSlide.classList.contains("active-circle")) {
+      circleSlide.classList.add("active-circle");
+      circleSlide.classList.remove("inactive-circle");
+      sliderBg.classList.add("active-bg");
+      monthyEl.classList.add("inactive-monthly");
+      yearlyEl.classList.add("active-yearly");
+    } else {
+      circleSlide.classList.remove("active-circle");
+      circleSlide.classList.add("inactive-circle");
+      sliderBg.classList.remove("active-bg");
+      monthyEl.classList.remove("inactive-monthly");
+      yearlyEl.classList.add("inactive-yearly");
+      yearlyEl.classList.remove("active-yearly");
+    }
+  });
+}
